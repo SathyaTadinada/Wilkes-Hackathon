3e5eb1c98c74b60fd176d70e056facc3612686b9
Coleman Rohde <rohdog2003@gmail.com>
2026-02-28 05:10:36 -0700

    added test

--- backend/test.py
+++ backend/test.py
@@ -1,16 +1,37 @@
-from Models import Model
+from Models import Model, Wind
 
-m = Model(84122, 0, 0, 0, 0, 0)
+m = Model(84122, 0, 0, 0, 0, 0, 0)
 print(m.getStation())
 print(m.getAverageWeatherData())
 print(m.getWeatherData("AWND"))
 
-m = Model(84043, 0, 0, 0, 0, 0)
+m = Model(84043, 0, 0, 0, 0, 0, 0)
 print(m.getStation())
 print(m.getAverageWeatherData())
 print(m.getWeatherData("AWND"))
 
-m = Model(84602, 0, 0, 0, 0, 0)
+m = Model(84602, 0, 0, 0, 0, 0, 0)
 print(m.getStation())
 print(m.getAverageWeatherData())
 print(m.getWeatherData("AWND"))
+
+m = Wind(84122, 0.08, 20000, 0, 0, 0, 10)
+print(m.getStation())
+print(m.getAverageWeatherData())
+print(m.getWeatherData("AWND"))
+print(m.Pi)
+print(m.NPVsub())
+
+m = Wind(84043, 0.08, 20000, 0, 0, 0, 10)
+print(m.getStation())
+print(m.getAverageWeatherData())
+print(m.getWeatherData("AWND"))
+print(m.Pi)
+print(m.NPVsub())
+
+m = Wind(84602, 0.08, 20000, 0, 0, 0, 10)
+print(m.getStation())
+print(m.getAverageWeatherData())
+print(m.getWeatherData("AWND"))
+print(m.Pi)
+print(m.NPVsub())
