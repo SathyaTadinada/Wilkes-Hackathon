2689961d6b23ac31db355882936c2d925769a19c
Coleman Rohde <rohdog2003@gmail.com>
2026-02-28 02:37:01 -0700

    finished wind, did not debug yet tho :(

--- backend/Models.py
+++ backend/Models.py
@@ -12,19 +12,8 @@ import scipy as sp
 weather_df = pd.read_csv("4244977.csv") # https://www.climate.gov/maps-data/dataset/past-weather-zip-code-data-table
 zip2station_df = pd.read_csv("zipcodes.csv")
 
-class House:
-    """"""
-    
-    def __init__(self):
-        """"""
-
-class TaxCredit():
-    """"""
-
 class Model:
     """"""
-    
-    percentReplacements = np.array([0.1, 0.3, 1])
     
     def __init__(self, zipcode: int, costperkWh: float, kWhperyear: float, costperBTU: float, BTUperyear: float, sqfeet: int, years: int):
         """"""
@@ -35,6 +24,7 @@ class Model:
         self.BTUperyear = BTUperyear
         self.sqfeet = sqfeet
         self.years
+        self.percentReplacements = np.array([0.1, 0.3, 1])
         self.station = self.getStation()
         self.totalAverageWeatherData = weather_df.drop(columns = ["STATION", "NAME"]).mean()
         self.averageWeatherData = self.getAverageWeatherData()
@@ -100,7 +90,6 @@ class Model:
         OM = self.OMcost(Pi, k_OM) # operating and management costs
         
         return sum(sav - OM for t in range(0, self.years))
-        
     
 class Solar(Model):
     """Solar energy solutions"""
@@ -118,23 +107,32 @@ class Wind(Model):
     def __init__(self, zipcode: int, costperkWh: float, kWhperyear: float, costperBTU: float, BTUperyear: float, sqfeet: int, years: int):
         """"""
         super().__init__(zipcode, costperkWh, kWhperyear, costperBTU, BTUperyear, sqfeet, years)
+        self.k_capex = 7850
+        self.k_OM = 46
+        self.Pa = self.kWhperyear/8760 * self.percentReplacements
+        self.Pi = self.powerRated()
     
-    def installCost(kW):
-        """https://solartechonline.com/blog/wind-turbine-cost-guide-2025/#:~:text=400W%20systems:%20$700%2D$850,only)%2C%20$80%2C000%2D$150%2C000%20installed"""
-        return 7850 * kW
+    def Area(self, P_rated):
+        """returns area in meters for a given power rating
+        https://docs.google.com/spreadsheets/d/1vxPFSOo5255XTnbqL5LU9BPplS1_HoSFddLY0MMfdlY/edit?usp=sharing"""
+        return 12.1 + 4.27 * np.log(P_rated)
     
-    def v_cutIN(self, P_rated):
-        """"""
-        return 1
+    def v_cutIn(self, P_rated):
+        """https://docs.google.com/spreadsheets/d/1D1LJACNSzSjRHw3l2GSSJ7E_lHYuaRjZIAY-oMghpIU/edit?usp=sharing"""
+        return 2.8 # m/s
     
     def v_rated(self, P_rated):
         """"""
-        return 1
+        eta = 0.3 # efficiency
+        rho = 1.225 # kg/m^3 https://en.wikipedia.org/wiki/Density_of_air sea level which is technically wrong for Utah
+        return (2 * 1000 * P_rated/(eta * rho * self.Area(P_rated)))**(1/3) # m/s
     
-    def power(v, P_rated, v_cutIn, v_rated, v_cutOut = np.inf):
+    def powerCurve(v, P_rated, v_cutIn, v_rated):
         """ wind speed (v) in mph. 
         https://energyeducation.ca/encyclopedia/Wind_power#:~:text=Wind%20speed%20largely%20determines%20the,electrical%20power%20from%20the%20generator.
         """
+        v_cutOut = np.inf
+        
         a = P_rated/(v_rated - v_cutIn)**3
         
         if v < v_cutIn:
@@ -145,9 +143,42 @@ class Wind(Model):
             return 1
         else:
             return 0
-        
     
+    def powerRated(self):
+        """"""
+        vms = 0.447 * self.getAverageWeatherData("AWND") # average daily wind speed in m/s
         
+        Pls = []
+        
+        for i, v in enumerate(vms):
+            func = lambda P_rated: Wind.powerCurve(vms, P_rated, self.v_cutIN(P_rated), self.v_rated(P_rated)) - self.powerActual[i]
+            a = 0.001 # kW
+            b = 100000 # kW
+            
+            if np.sign(func(a)) != np.sign(func(b)):
+                P_r = sp.optimize.brentq(func, a, b)
+            else:
+                P_r = np.nan
+                
+        Pls.append(P_r)
+            
+        return np.array(Pls)
+    
+    def installCost(self):
+        """https://solartechonline.com/blog/wind-turbine-cost-guide-2025/#:~:text=400W%20systems:%20$700%2D$850,only)%2C%20$80%2C000%2D$150%2C000%20installed"""
+        return super().installCost(self.Pi, self.k_capex)
+    
+    def OMcost(self):
+        """https://www.energy.gov/sites/default/files/2022-08/distributed_wind_market_report_2022.pdf?utm_source=chatgpt.com"""
+        return super().OMcost(self.Pi, self.k_OM)
+    
+    def NPV(self):
+        """"""
+        return [super().NPV(self.Pa[i], self.Pi[i], self.k_capex, self.k_OM) for i in range(0, len(self.percentReplacements))]
+        
+    def savingsOverTime(self):
+        return [super().savingsOverTime(self.Pa[i], self.Pi[i], self.k_capex, self.k_OM) for i in range(0, len(self.percentReplacements))]
+    
 class Geo(Model):
     """Geothermal energy solutions"""
     
