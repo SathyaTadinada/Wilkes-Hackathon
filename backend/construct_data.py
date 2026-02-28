0206766a7c190f34de6af0810ff827091aaccb27
William Lee <williamwooseoklee@gmail.com>
2026-02-28 01:14:19 -0700

    build_dataset integrates with models objects and functions

--- backend/construct_data.py
+++ backend/construct_data.py
@@ -1,16 +1,31 @@
 import pandas as pd
+import Models
 
 def build_dataset():
-    pass
+    energy_objects = []
+    method = []
+    installation_cost = []
+    npv = []
+    for energy_object in energy_objects:
+        method.append(energy_object.name)
+        installation_cost.append(energy_object.installCost())
+        npv.append(energy_object.NPV())
+
+    df = pd.DataFrame({
+            "Method": method,
+            "NPV": npv,
+            "Installation Cost": installation_cost,
+        })
+
+    return df
 
 def convert_dataset():
     #df = build_dataset()
     df = pd.DataFrame({
         "Method": ["Solar", "Wind", "Geothermal"],
-        "Installation Cost": [100, 200, 300],
-        "Cost Per Year": [200, 300, 400],
         "NPV": [100, 300, 200]
+        "Installation Cost": [100, 200, 300],
     })
-    df_sorted = df.sort_values(by="age")
+    df_sorted = df.sort_values(by="NPV")
     df_json = df_sorted.to_json()
     return df_json
