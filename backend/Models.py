4f09033a7760ee36460a1cbd5981f588b74296e5
Coleman Rohde <rohdog2003@gmail.com>
2026-02-27 20:28:34 -0700

    weather data read in

--- backend/Models.py
+++ backend/Models.py
@@ -24,14 +24,17 @@ class TaxCredit():
 class Model:
     """"""
     
-    def __init__(self, zipcode: int, costperkWh: float, kWhperyear: float, costperBTU: float, BTUperyear: float):
+    def __init__(self, zipcode: int, costperkWh: float, kWhperyear: float, costperBTU: float, BTUperyear: float, sqfeet: int):
         """"""
         self.zipcode = zipcode
         self.costperkWh = costperkWh
         self.kWhperyear = kWhperyear
         self.costperBTU = costperBTU
         self.BTUperyear = BTUperyear
+        self.sqfeet = sqfeet
         self.station = self.getStation()
+        self.totalAverageWeatherData = weather_df.drop(columns = ["STATION", "NAME"]).mean()
+        self.averageWeatherData = self.getAverageWeatherData()
         
     def getStation(self):
         """"""
@@ -45,38 +48,48 @@ class Model:
         else:
             return s[0]
         
-    def averageData(self):
+    def getAverageWeatherData(self):
         """"""
         if self.station is None:
-            return weather_df.drop(columns = ["STATION", "NAME"]).mean()
+            return self.totalAverageWeatherData
         else:
             return weather_df.loc[weather_df["STATION"] == self.station].drop(columns = ["STATION", "NAME"]).mean()
+        
+    def getWeatherData(self, dataStr):
+        """"""
+        data = self.averageWeatherData[dataStr]
+        
+        print(np.isnan(data))
+        if np.isnan(data):
+            return self.totalAverageWeatherData[dataStr]
+        else:
+            return data
         
         
 class Solar(Model):
     """Solar energy solutions"""
     
-    def __init__(self, zipcode, budget):
+    def __init__(self, zipcode: int, costperkWh: float, kWhperyear: float, costperBTU: float, BTUperyear: float, sqfeet: int):
         """"""
-        super().__init__(zipcode, budget)
+        super().__init__(zipcode, costperkWh, kWhperyear, costperBTU, BTUperyear, sqfeet)
         
 class Wind(Model):
     """Wind energy solutions"""
     
-    def __init__(self, zipcode, budget):
+    def __init__(self, zipcode: int, costperkWh: float, kWhperyear: float, costperBTU: float, BTUperyear: float, sqfeet: int):
         """"""
-        super().__init__(zipcode, budget)
+        super().__init__(zipcode, costperkWh, kWhperyear, costperBTU, BTUperyear, sqfeet)
         
 class Geo(Model):
     """Geothermal energy solutions"""
     
-    def __init__(self, zipcode, budget):
+    def __init__(self, zipcode: int, costperkWh: float, kWhperyear: float, costperBTU: float, BTUperyear: float, sqfeet: int):
         """"""
-        super().__init__(zipcode, budget)
+        super().__init__(zipcode, costperkWh, kWhperyear, costperBTU, BTUperyear, sqfeet)
         
 class DSM(Model):
     """Demand Side Management (DSM) solutions"""
     
-    def __init__(self, zipcode, budget):
+    def __init__(self, zipcode: int, costperkWh: float, kWhperyear: float, costperBTU: float, BTUperyear: float, sqfeet: int):
         """"""
-        super().__init__(zipcode, budget)
+        super().__init__(zipcode, costperkWh, kWhperyear, costperBTU, BTUperyear, sqfeet)
---
+++ backend/test.py
@@ -0,0 +1,16 @@
+from Models import Model
+
+m = Model(84122, 0, 0, 0, 0, 0)
+print(m.getStation())
+print(m.getAverageWeatherData())
+print(m.getWeatherData("AWND"))
+
+m = Model(84043, 0, 0, 0, 0, 0)
+print(m.getStation())
+print(m.getAverageWeatherData())
+print(m.getWeatherData("AWND"))
+
+m = Model(84602, 0, 0, 0, 0, 0)
+print(m.getStation())
+print(m.getAverageWeatherData())
+print(m.getWeatherData("AWND"))
